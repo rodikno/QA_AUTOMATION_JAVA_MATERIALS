06fa75c120f32d5540fd9408db5684d00af79d28
Rodion Baronov <rodikno@gmail.com>
2024-06-21 02:43:13 +0300

    Removed unneded old files

---
+++ Lesson_2_1_Java_Intro/NewProject/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, AUTO QA 1.0!");
+
+        int myNumber = 31;
+        System.out.println("My number is: " + myNumber);
+        byte num = 100;
+        long longNym = 1_000_000_000L;
+
+        char letter = 45;
+
+        System.out.println("The letter is: " + letter);
+    }
+}
